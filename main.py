02fdd6c0a74c86d2310e561a6b5c463d8e7230d5
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-06-02 15:18:21 +0300

    Add batching reading

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ def select_audio_file():
 
 
 def plot_audio_spectrum(filename):
+    chunk_size = 1024  # Размер блока данных для построения графика
+
     # Открываем аудиофайл
     with wave.open(filename, 'rb') as wav_file:
         # Получаем параметры аудиофайла
@@ -26,32 +28,43 @@ def plot_audio_spectrum(filename):
         sample_rate = wav_file.getframerate()
         num_frames = wav_file.getnframes()
 
-        # Читаем аудиоданные
-        audio_data = wav_file.readframes(num_frames)
+        # Вычисляем длительность и количество блоков данных
+        duration = num_frames / sample_rate
+        num_blocks = int(num_frames / chunk_size)
 
-    # Преобразуем байтовые данные в массив NumPy
-    if sample_width == 2:
-        audio_data = np.frombuffer(audio_data, dtype=np.int16)
-    elif sample_width == 4:
-        audio_data = np.frombuffer(audio_data, dtype=np.int32)
+        # Создаем график
+        plt.figure(figsize=(10, 4))
+        plt.xlabel('Time [s]')
+        plt.ylabel('Amplitude')
+        plt.title('Audio Spectrum')
+        plt.grid(True)
 
-    # Вычисляем время для оси x
-    duration = num_frames / sample_rate
-    time = np.linspace(0, duration, num=len(audio_data))
+        # Читаем и поэтапно строим график для каждого блока данных
+        for block_index in range(num_blocks):
+            # Читаем блок данных
+            audio_data = wav_file.readframes(chunk_size)
 
-    # Создаем график
-    plt.figure(figsize=(10, 4))
-    plt.plot(time, audio_data)
-    plt.xlabel('Time [s]')
-    plt.ylabel('Amplitude')
-    plt.title('Audio Spectrum')
-    plt.grid(True)
-    plt.show()
+            # Преобразуем байтовые данные в массив NumPy
+            if sample_width == 2:
+                audio_data = np.frombuffer(audio_data, dtype=np.int16)
+            elif sample_width == 4:
+                audio_data = np.frombuffer(audio_data, dtype=np.int32)
+
+            # Вычисляем время для оси x
+            time = np.linspace(
+                block_index * chunk_size / sample_rate,
+                (block_index + 1) * chunk_size / sample_rate,
+                num=len(audio_data)
+            )
+
+            # Построение графика
+            plt.plot(time, audio_data, color='blue')
+
+        plt.show()
 
 
 # Выбираем аудиофайл
 filename = select_audio_file()
-
 if filename:
     # Показываем амплитудно-частотную характеристику выбранного аудиофайла
     plot_audio_spectrum(filename)
